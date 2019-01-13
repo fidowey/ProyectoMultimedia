3e03de9174b52935fc9e078ba84eb97ec7a5b115
LeoneSerker <leonesti.dz@gmail.com>
2019-01-13 03:20:46 -0300

    UPDATE

--- views/include/footer.php
+++ views/include/footer.php
@@ -1,5 +1,28 @@
+
+<div class="container">
+		<style>
+		#footer{
+		background-color:#dcedf5; 
+		}
+		</style>
+		<div id="footer" class="row">
+		<div class="col-6">	
+			<img src="views/include/img/conaf_logo.png" width="128" height="128" alt="conaf_logo">
+			
+		</div>
+		<div class="col-2"></div>
+		<div class="col-4">
+			CONAF: Corporación Nacional Forestal
+			<br>
+			Paseo Bulnes 285
+			<br>
+			Santiago, Chile
+			<br>
+			Fono: +56(2)26630000
+			<br>
+			Email: consulta.oirs@conaf.cl
+		</div>
+</div>
 </body>
-	<script src="js/jquery.min.js" ></script>
-	<script src="js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/bootstrap.css">
+
 </html>
