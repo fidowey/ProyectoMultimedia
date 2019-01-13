6165e427a71753dedd5555b3be0878377e3cbbd0
LeoneSerker <leonesti.dz@gmail.com>
2019-01-13 01:56:41 -0300

    UPDATE

--- bootstrap_link.php
+++ bootstrap_link.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
 	<link rel="stylesheet" href="views/include/css/bootstrap.css">
-</head>
-<body>
-	
-</body>
+
 	<script src="views/include/js/jquery.min.js" ></script>
 	<script src="views/include/js/bootstrap.min.js"></script>
 </html>
