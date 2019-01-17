a04cbebdd3dc495301fbc6fe6dfbcc1a224baadf
LeoneSerker <leonesti.dz@gmail.com>
2019-01-16 23:29:44 -0300

    UPDATE

--- views/contacto.php
+++ views/contacto.php
@@ -1,3 +1,5 @@
 <?php require_once "include/head.php"; ?>
+	<?php include_once "include/bootstrap_link_views.php"; ?>
+
 
 <?php require_once "include/footer.php"; ?>
---
+++ views/include/bootstrap_link_views.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+	<link rel="stylesheet" href="include/css/bootstrap.css">
+
+	<script src="include/js/jquery.min.js" ></script>
+	<script src="include/js/bootstrap.min.js"></script>
+</html>
