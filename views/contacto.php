c9d0ca49edf7ebe3853b1a1d42f47a9b850ba2a3
LeoneSerker <leonesti.dz@gmail.com>
2019-01-16 23:40:47 -0300

    UPDATE

--- views/contacto.php
+++ views/contacto.php
@@ -1,5 +1,5 @@
-<?php require_once "include/head.php"; ?>
-	<?php include_once "include/bootstrap_link_views.php"; ?>
+<?php require_once "include/head_views.php"; ?>
+	
 
 
-<?php require_once "include/footer.php"; ?>
+<?php require_once "include/footer_views.php"; ?>
---
+++ views/include/head_views.php
@@ -0,0 +1,100 @@
+	<?php include_once "include/bootstrap_link_views.php"; ?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title></title>
+
+	</head>
+	<body>
+	<div class="container">
+		<div class="row">
+			<div id="margen" class="col-12">
+				<br>
+				<br>
+				<br>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<img src="include/img/conaf_logo2.png" class="img-responsive" width="369" height="128" alt="conaf_logo2">
+			</div>
+			<div class="col-6" style="text-align: center;">
+				<br>
+			<p><h1>BIENVENIDO</h1></p>
+			<br>				
+			</div>
+			<div class="col-3"></div>
+		</div>
+		<div class="row">
+					<style>
+					#margen{
+    				text-align: center;
+    				background-color:#dcedf5;
+    				}
+    				#Inicio {
+    				text-align: center;
+    				background-color:#458def;
+    				}
+    				#Parques {
+    				text-align: center;
+    				background-color:#58b5ab;
+    				}
+    				#Contacto {
+    				text-align: center;
+    				background-color:#a076cc;
+    				}
+    				#Nosotros {
+    				text-align: center;
+    				background-color:#5abd51;
+    				}
+    				#Visitanos {
+    				text-align: center;
+    				background-color:#54b5e2;
+    				}
+    				#Iniciar_Sesion {
+    				text-align: center;
+    				background-color:#f38f31;
+    				}    				    				
+    				</style>
+
+  					<div id="Inicio" class="col-2" >
+      				<a class="nav-item nav-link text-light" href="index.php">Inicio</a>
+      				</div>
+      				<div id="Parques" class="col-2">
+      				<a class="nav-item nav-link text-light" href="#">Parques</a>
+      				</div>
+      				<div id="Contacto" class="col-2">
+      				<a class="nav-item nav-link text-light" href="#">Contacto</a>
+      				</div>
+      				<div id="Nosotros" class="col-2">
+      				<a class="nav-item nav-link text-light" href="#">Nosotros</a>
+      				</div>
+      				<div id="Visitanos" class="col-2 ">
+      				<a class="nav-item nav-link text-light" href="#">Visitanos</a>
+      				</div>
+      				<div id="Iniciar_Sesion" class="col-2">
+      				<a class="nav-item nav-link text-light" href="#">Iniciar Sesión</a>
+      				</div>
+		</div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  			<button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+  			</button>
+  			<div class="collapse navbar-collapse bg-info" id="navbarSupportedContent">
+    			<ul class="navbar-nav mr-auto text-center ">
+      				<li class="nav-item active">
+        				<a class="nav-link text-center" href="#">	Inicio		</a>
+      				</li>
+      				<li class="nav-item ">
+        				<a class="nav-link text-center" href="/ProyectoMultimedia/views/contacto.php">	Contacto	</a>
+      				</li>
+      				<li class="nav-item">
+        				<a class="nav-link text-center" href="#">	Nosotros	</a>
+      				</li>
+      				<li class="nav-item">
+        				<a class="nav-link text-center " href="#">	Visitanos	</a>
+      				</li>
+    			</ul>
+				<a href="#!" class="btn btn-primary btn-lg active bg-success" role="button" aria-pressed="true">Iniciar sesion</a>
+  			</div>
+		</nav>
