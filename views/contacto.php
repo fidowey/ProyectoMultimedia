6e776967a45407d68016f320273360cfd95faf2e
fidowey <41456964+fidowey@users.noreply.github.com>
2019-01-18 12:26:06 -0300

    se termino archivo "contacto"

--- views/contacto.php
+++ views/contacto.php
@@ -1,33 +1,54 @@
 <?php require_once "include/head_views.php"; ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
 
   <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    <div class="col-md-4">
+		<img src="include/img/forestin.png" class="img-fluid rounded mx-auto d-block">
     </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    <div class="col-md-4">
+		<form action="correo.php">
+    		<div class="form-group">
+      			<label >Nombre</label>
+      			<input type="text"  class="form-control" name="nombre">
+    		</div>
+    		<div class="form-group">
+      			<label >Direccion</label>
+      			<input type="text"  class="form-control" name="direccion">
+    		</div>
+    		<div class="form-group">
+      			<label >Telefono</label>
+      			<input type="text"  class="form-control" name="telefono">
+    		</div>
+    		<div class="form-group">
+      			<label >Email</label>
+      			<input type="text"  class="form-control" name="email" >
+    		</div>
+    		<div class="form-group">
+      			<label >Motivo de consulta</label>
+      			<select  class="form-control" name="motivo">
+        		<option>sugerencia</option>
+        		<option>consulta</option>
+        		<option>reclamo</option>
+      			</select>
+    		</div>
+			<div class="form-group">
+    			<label for="exampleFormControlTextarea1">Mensaje</label>
+    			<textarea class="form-control" name="mensaje" rows="3"></textarea>
+  			</div>
+  	    	<div class="form-check">
+      			<input class="form-check-input" type="checkbox" >
+      			<label class="form-check-label" >
+        		check
+      			</label>
+    		</div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+
+</form>
     </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    <div class="col-md-4">
+		<img src="include/img/conaf.jpg" class="img-fluid rounded mx-auto d-block" >
     </div>
   </div>
-
-
-</html>
 	
 
 
---
+++ views/login.php
@@ -0,0 +1,3 @@
+<?php require_once "include/head_views.php"; ?>
+
+<?php require_once "include/footer_views.php"; ?>
