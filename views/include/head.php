44ab9de398991a4cd02af3d37dc578a7f56860b3
LeoneSerker <leonesti.dz@gmail.com>
2019-01-18 23:24:04 -0300

    UPDATE

--- views/include/head.php
+++ views/include/head.php
@@ -33,7 +33,7 @@
   			<div class="collapse navbar-collapse bg-info" id="navbarSupportedContent">
     			<ul class="navbar-nav mr-auto text-center ">
       				<li class="nav-item active">
-        				<a class="nav-link text-center" href="/ProyectoMultimedia/views/inicio.php">	Inicio		</a>
+        				<a class="nav-link text-center" href="index.php">	Inicio		</a>
       				</li>
       				<li class="nav-item ">
         				<a class="nav-link text-center" href="/ProyectoMultimedia/views/contacto.php">	Contacto	</a>
