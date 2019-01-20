05a70157b451d368cb39f9894f3670b0b3c0fe23
LeoneSerker <leonesti.dz@gmail.com>
2019-01-20 00:42:37 -0300

    Update registroUsuario.php

--- controller/registroUsuario.php
+++ controller/registroUsuario.php
@@ -6,5 +6,15 @@
 	$dv = $_POST['dv'];
 	$fechaNac=$_POST['fechaNac'];
 	$sexo=$_POST['sexo'];
+	$pasaporte=$_POST['pasaporte'];
+	$telefono=$_POST['telefono'];
+
+
+	$hoy=getdate(); //obtenemos la fecha actual
+	$anioactual=date("Y",$hoy); //extraemos el año de la fecha actual
+	$anionac= date("Y",$fechaNac); //extraemos el año de la fecha de nacimiento
+
+	$edad=$anioactual-$anionac; //hacemos el calculo de edad mediante una operacion matemática
+
 	
  ?>
