e4123fa98d8be0cbd017c5db7fba8460f1733464
LeoneSerker <leonesti.dz@gmail.com>
2019-03-04 00:16:02 -0300

    update
    he añadido solo por si acaso el codigo de arduino

--- views/inicio.php
+++ views/inicio.php
@@ -1,4 +1,5 @@
 <?php require_once "include/head.php"; ?>
+
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
@@ -6,8 +7,8 @@
 					<img class="card-img-top" src="views/include/img/el-peral.jpg" alt="Card image cap">
 					<div class="card-body">
 						<h5 class="card-title"> Laguna El Peral</h5>
-						<p class="card-text"> temperatura: 25°</p>
-						<p class="card-text"> Dia: despejado</p>
+						<p class="card-text"> temperatura: </p>
+						<p class="card-text"> Humedad: </p>
 						<a href="/ProyectoMultimedia/views/peral.php" class="btn btn-primary">Ver mas</a>
 					</div>
 				</div>
@@ -17,8 +18,8 @@
 					<img class="card-img-top" src="views/include/img/la-campana.jpg" alt="Card image cap">
 					<div class="card-body">
 						<h5 class="card-title">Parque Nacional La Campana</h5>
-						<p class="card-text"> temperatura: 19°</p>
-						<p class="card-text"> Dia: parcialmente nublado</p>
+						<p class="card-text"> Temperatura: <span id="tempVal"></span></p>
+						<p class="card-text"> Humedad: <span id="uptimeVal"></span></p>
 						<a href="/ProyectoMultimedia/views/campana.php" class="btn btn-primary">Ver mas </a>
 					</div>
 				</div>
@@ -28,8 +29,8 @@
 					<img class="card-img-top" src="views/include/img/peñuela.jpg" alt="Card image cap">
 					<div class="card-body">
 						<h5 class="card-title">Lago Peñuelas</h5>
-						<p class="card-text"> temperatura: 25°</p>
-						<p class="card-text"> Dia: lluvia</p>
+						<p class="card-text"> Temperatura: 25°</p>
+						<p class="card-text"> Humedad: </p>
 						<a href="/ProyectoMultimedia/views/peñuelas.php" class="btn btn-primary">Ver mas</a>
 					</div>
 				</div>
